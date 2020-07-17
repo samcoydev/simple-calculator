514d0817a53ddafd771b754f33750f780d04ef88
samcoydev <samuelcoydev@gmail.com>
2020-07-17 14:24:08 -0500

    Basic calculations done

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -6,73 +6,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
+
+  currentNumber = '0';
+  currentEquation;
+  firstOperand = null;
+  operator = null;
+  waitForSecondNumber = false;
+
+  public getNumber(v: string){
+    console.log(v);
+    if(this.waitForSecondNumber)
+    {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    }else{
+      this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
+
+    }
+  }
+
+  getDecimal(){
+    if(!this.currentNumber.includes('.')){
+        this.currentNumber += '.'; 
+    }
+  }
+
+  private doCalculation(op , secondOp){
+    switch (op){
+      case '+':
+      return this.firstOperand += secondOp; 
+      case '-': 
+      return this.firstOperand -= secondOp; 
+      case '*': 
+      return this.firstOperand *= secondOp; 
+      case '/': 
+      return this.firstOperand /= secondOp; 
+      case '=':
+      return secondOp;
+    }
+  }
+
+  public getOperation(op: string){
+    console.log(op);
+
+    if(this.firstOperand === null){
+      this.firstOperand = Number(this.currentNumber);
+
+    }else if(this.operator){
+      const result = this.doCalculation(this.operator , Number(this.currentNumber))
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+
+    this.currentEquation = 'firstOperand' + op + 'secondOperand';
+
+    console.log(this.firstOperand);
+
+  }
+
+  public clear(){
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
-
-  subText = ''; // The text that should appear in the sub-display
-  mainText = ''; // The text that should appear in the main display
-  operand1: number; // The first operand
-  operand2: number; // The second operand
-  operator = ''; // The operator
-  calculationString = ''; // This is the string that denotes the operation being performed
-  answered = false;  // A flag to check whether the solution has been processed
-  operatorSet = false; // You'll see how this is used soon
-
-  pressKey(key: string) {
-    if (key === '/' || key === 'x' || key === '-' || key === '+') {
-       const lastKey = this.mainText[this.mainText.length - 1];
-       if (lastKey === '/' || lastKey === 'x' || lastKey === '-' || lastKey === '+')  {
-         this.operatorSet = true;
-       }
-       if ((this.operatorSet) || (this.mainText === '')) {
-         return;
-       }
-       this.operand1 = parseFloat(this.mainText);
-       this.operator = key;
-       this.operatorSet = true;
-    }
-    if (this.mainText.length === 10) {
-      return;
-    }
-    this.mainText += key;
- }
-
- getAnswer() {
-  this.calculationString = this.mainText;
-  this.operand2 = parseFloat(this.mainText.split(this.operator)[1]);
-  if (this.operator === '/') {
-    this.subText = this.mainText;
-    this.mainText = (this.operand1 / this.operand2).toString();
-    this.subText = this.calculationString;
-    if (this.mainText.length > 9) {
-      this.mainText = this.mainText.substr(0, 9);
-    }
-  } else if (this.operator === 'x') {
-    this.subText = this.mainText;
-    this.mainText = (this.operand1 * this.operand2).toString();
-    this.subText = this.calculationString;
-    if (this.mainText.length > 9) {
-      this.mainText = 'ERROR';
-      this.subText = 'Range Exceeded';
-    }
-  } else if (this.operator === '-') {
-    this.subText = this.mainText;
-    this.mainText = (this.operand1 - this.operand2).toString();
-    this.subText = this.calculationString;
-  } else if (this.operator === '+') {
-    this.subText = this.mainText;
-    this.mainText = (this.operand1 + this.operand2).toString();
-    this.subText = this.calculationString;
-    if (this.mainText.length > 9) {
-      this.mainText = 'ERROR';
-      this.subText = 'Range Exceeded';
-    }
-  } else {
-    this.subText = 'ERROR: Invalid Operation';
-  }
-  this.answered = true;
-}
 
 }
