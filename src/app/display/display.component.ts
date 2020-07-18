083046c7e185198d5e36be0470fbaba530e62dbb
samcoydev <samuelcoydev@gmail.com>
2020-07-18 17:58:44 -0500

    Calculation works. Displays the problem as you type it. Need to fix the clearing function

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -19,48 +19,53 @@ export class DisplayComponent implements OnInit {
   operator = null;
   waitForSecondNumber = false;
 
+  firstNumberSelected = true;
+
   // Create a function to determine if currentOp is being used or not.
   // Then we can make a function that switches which number variable you're using
   // by stopping the number when an operation is selected.
 
-  // Called anytime a number button is pressed.
-  public getNumber(v: string){
-    console.log(v);
+  // When operator is selected switch numbers.
+  // Make equal a different type than operator.
 
+  // Called anytime a number button is pressed.
+  public getNumber(value: string){
+    //console.log("GETNUMBER FUNCTION - ", value);
     if(this.waitForSecondNumber)
     {
-      this.currentNumber = v;
+      this.currentNumber = value;
       this.waitForSecondNumber = false;
-      this.secondNum = String(v);
-    }else{
-      this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
-      this.firstNum = this.currentNumber;
+    } else {
+      // This line adds the values together in a string. So instead of 5+5 it becomes 55.
+      this.currentNumber === '0'? this.currentNumber = value: this.currentNumber += value;
     }
-
+    this.setNumber();
     this.updateEquation();
   }
 
   // Called anytime a = - * % button is pressed.
   public getOperation(op: string){
     if (op != '=') {
+      this.firstNumberSelected = false;
       this.currentOp = op;
+      this.waitForSecondNumber = true;
       this.updateEquation();
+    } else {
+        this.currentOp = '';
     }
-
-    if(this.firstOperand === null){
-      this.firstOperand = Number(this.currentNumber);
-
-    }else if(this.operator){
-      const result = this.doCalculation(this.operator , Number(this.currentNumber))
-      this.currentNumber = String(result);
-      this.firstOperand = result;
-      this.firstNum = result;
-      this.secondNum = ' ';
-      this.currentEquation = result;
-    }
+    
     this.operator = op;
-    this.waitForSecondNumber = true;
   } 
+
+  public setNumber() {
+    if (this.firstNumberSelected) {
+      this.firstNum = this.currentNumber;
+      this.firstOperand = Number(this.currentNumber);
+    } else {
+      this.secondNum = this.currentNumber;
+    } 
+  }
+
   
   public updateEquation() {
     if (this.firstNum == null) {
@@ -68,9 +73,10 @@ export class DisplayComponent implements OnInit {
     } else {
       this.currentEquation = this.firstNum + this.currentOp + this.secondNum;
     }
-
-    console.log('CURRENT NUMBER', this.currentNumber);
     console.log('CURRENT EQUATION', this.currentEquation);
+    console.log('CURRENT EQUATION - Current number is: ', this.currentNumber);
+    console.log('CURRENT EQUATION - Current first number is:', this.firstNum);
+    console.log('CURRENT EQUATION - Current second number is:', this.secondNum);
   }
 
   getDecimal(){
@@ -79,9 +85,21 @@ export class DisplayComponent implements OnInit {
     }
   }
 
+  public solveEquation() {
+    console.log('= pressed');
+    const result = this.doCalculation(this.operator , Number(this.currentNumber))
+    this.currentNumber = result;
+    this.firstNum = result;
+    this.secondNum = '';
+    this.operator = '';
+    this.currentOp = '';
+    this.waitForSecondNumber = false;
+    this.firstNumberSelected = true;
+    this.updateEquation();
+  }
 
   private doCalculation(op , secondOp){
-
+    console.log('on docalculation ', this.firstOperand);
     switch (op){
       case '+':
       return this.firstOperand += secondOp; 
@@ -98,9 +116,13 @@ export class DisplayComponent implements OnInit {
 
   public clear(){
     this.currentNumber = '0';
+    this.firstNum = '0';
+    this.secondNum = '';
+    this.currentOp = '';
     this.firstOperand = null;
     this.operator = null;
     this.waitForSecondNumber = false;
+    this.firstNumberSelected = true;
   }
 
   constructor() { }
